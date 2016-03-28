2126a876557341943d1c8a35b163cefb52c8e977
yanweiqing <yanweiqing@youzan.com>
2016-03-28 11:57:56 +0800

    小优化

--- src/pickertime.js
+++ src/pickertime.js
@@ -2,46 +2,63 @@ import React, { Component } from 'react';
 
 class PickerTime extends Component {
 
-    _handleClick = (timeType, num) => {
-        this.props.timeSwitch(num, timeType);
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    calcute(time, type) {
+        switch (type) {
+            case 'hour':
+                return time.getHours();
+                break;
+            case 'minute':
+                return time.getMinutes();
+                break;
+            case 'seconds':
+                return time.getSeconds();
+                break;
+        }
+    }
+
+    handleClick(timeType) {
+        // 此处想办法用dom方法传参数
+        this.props.timeSwitch(this.calcute(this.props.time, timeType), timeType);
     }
 
     render() {
         if ((this.props.dateOnly === true && this.props.timeOnly !== true) || this.props.type !== 'day') {
             return null;
         } else {
-            let time = this.props.time,
-                hour = time.getHours(),
-                minute = time.getMinutes(),
-                seconds = time.getSeconds();
+            let { time } = this.props;
 
             return (
                 <div className="salt-calendar-cell-footer clearfix">
                     <div className="salt-calendar-cell salt-calendar-cell-time">
                         <button
                             className="btn btn-link btn-block"
-                            onClick={this._handleClick.bind(this, 'hour', hour)}
+                            onClick={this.handleClick.bind(null, 'hour')}
                             type="button"
                         >
-                            {hour}时
+                            {this.calcute(time, 'hour')}时
                         </button>
                     </div>
                     <div className="salt-calendar-cell salt-calendar-cell-time">
                         <button
                             className="btn btn-link btn-block"
-                            onClick={this._handleClick.bind(this, 'minute', minute)}
+                            onClick={this.handleClick.bind(null, 'minute')}
                             type="button"
                         >
-                            {minute}分
+                            {this.calcute(time, 'minute')}分
                         </button>
                     </div>
                     <div className="salt-calendar-cell salt-calendar-cell-time">
                         <button
                             className="btn btn-link btn-block"
-                            onClick={this._handleClick.bind(this, 'seconds', seconds)}
+                            onClick={this.handleClick.bind(null, 'seconds')}
                             type="button"
                         >
-                            {seconds}秒
+                            {this.calcute(time, 'seconds')}秒
                         </button>
                     </div>
                 </div>
